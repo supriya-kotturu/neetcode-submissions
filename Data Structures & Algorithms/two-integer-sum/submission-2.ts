dd2510eaa828f24ebb8ee1a93a946a843b56051f
supriya <35397239+supriya-kotturu@users.noreply.github.com>
2026-09-07 18:30:02 -0700

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.ts
@@ -0,0 +1,16 @@
+class Solution {
+    twoSum(nums: number[], target: number): number[] {
+        const diffMap = new Map<number, number>()
+
+        for(let idx = 0; idx < nums.length; idx++){
+            const n = nums[idx];
+            const diff = target - n
+            if(diffMap.has(diff)){
+                return [idx, diffMap.get(diff)]
+            }
+            diffMap.set(n, idx)
+        }
+
+        return [-1, -1]
+    }
+}
